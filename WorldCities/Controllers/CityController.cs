9bbf567973e80b838b5d902c1524975ac5df3cc0
Vladimir Ivanov <vgivanov@gmail.com>
2022-02-06 19:11:18 +0200

    RESTFULL Complete

---
+++ WorldCities/Controllers/CityController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WorldCities.Data;
+using WorldCities.Data.Models;
+
+namespace WorldCities.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        private readonly ApplicationDbContext context;
+        public CitiesController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/Cities
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<City>>> GetCities()
+        {
+            return await context.Cities.ToListAsync();
+        }
+
+        // GET: api/Cities/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<City>> GetCity(int id)
+        {
+            var city = await context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            return city;
+        }
+
+        // PUT: api/Cities/5
+        // To protect from overposting attacks, please enable the
+        // specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCity(int id, City city)
+        {
+            if (id != city.Id)
+            {
+                return BadRequest();
+            }
+
+            context.Entry(city).State = EntityState.Modified;
+
+            try
+            {
+                await context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!CityExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        // POST: api/Cities
+        // To protect from overposting attacks, please enable the
+        // specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<City>> PostCity(City city)
+        {
+            context.Cities.Add(city);
+            await context.SaveChangesAsync();
+            return CreatedAtAction("GetCity", new { id = city.Id }, city);
+        }
+
+        // DELETE: api/Cities/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<City>> DeleteCity(int id)
+        {
+            var city = await context.Cities.FindAsync(id);
+            if (city == null)
+            {
+                return NotFound();
+            }
+            context.Cities.Remove(city);
+            await context.SaveChangesAsync();
+            return city;
+        }
+        private bool CityExists(int id)
+        {
+            return context.Cities.Any(e => e.Id == id);
+        }
+    }
+}
