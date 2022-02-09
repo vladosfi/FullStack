638edf80b40184985c8aacdb3d8d2208646afc47
vladosfi <vladosfi@gmail.com>
2022-02-09 17:11:42 +0200

    Add Filtering

--- WorldCities/Controllers/CityController.cs
+++ WorldCities/Controllers/CityController.cs
@@ -25,9 +25,11 @@ namespace WorldCities.Controllers
                int pageIndex = 0,
                int pageSize = 10,
                string sortColumn = null,
-               string sortOrder = null)
+               string sortOrder = null,
+               string filterColumn = null,
+               string filterQuery = null)
         {
-            return await ApiResult<City>.CreateAsync(context.Cities, pageIndex, pageSize, sortColumn, sortOrder);
+            return await ApiResult<City>.CreateAsync(context.Cities, pageIndex, pageSize, sortColumn, sortOrder, filterColumn, filterQuery);
         }
 
         // GET: api/Cities/5
--- WorldCities/Data/ApiResult.cs
+++ WorldCities/Data/ApiResult.cs
@@ -13,7 +13,15 @@ namespace WorldCities.Data
         /// <summary>
         /// Private constructor called by the CreateAsync method.
         /// </summary>
-        private ApiResult(List<T> data, int count, int pageIndex, int pageSize, string sortColumn, string sortOrder)
+        private ApiResult(
+            List<T> data,
+            int count,
+            int pageIndex,
+            int pageSize,
+            string sortColumn,
+            string sortOrder,
+            string filterColumn,
+            string filterQuery)
         {
             Data = data;
             PageIndex = pageIndex;
@@ -22,23 +30,49 @@ namespace WorldCities.Data
             TotalPages = (int)Math.Ceiling(count / (double)pageSize);
             SortColumn = sortColumn;
             SortOrder = sortOrder;
+            FilterColumn = filterColumn;
+            FilterQuery = filterQuery;
         }
         #region Methods
         /// <summary>
-        /// Pages a IQueryable source.
+        /// Pages, sorts and/or filters a IQueryable source.
         /// </summary>
         /// <param name="source">An IQueryable source of generic
         /// type</param>
         /// <param name="pageIndex">Zero-based current page index
         /// (0 = first page)</param>
-        /// <param name="pageSize">The actual size of each
-        /// page</param>
+        /// <param name="pageSize">The actual size of
+        /// each page</param>
+        /// <param name="sortColumn">The sorting column name</param>
+        /// <param name="sortOrder">The sorting order ("ASC" or
+        /// "DESC")</param>
+        /// <param name="filterColumn">The filtering column
+        /// name</param>
+        /// <param name="filterQuery">The filtering query (value to
+        /// lookup)</param>
         /// <returns>
-        /// A object containing the paged result
-        /// and all the relevant paging navigation info.
+        /// A object containing the IQueryable paged/sorted/filtered
+        /// result
+        /// and all the relevant paging/sorting/filtering navigation
+        /// info.
         /// </returns>
-        public static async Task<ApiResult<T>> CreateAsync(IQueryable<T> source, int pageIndex, int pageSize, string sortColumn = null, string sortOrder = null)
+        public static async Task<ApiResult<T>> CreateAsync(
+            IQueryable<T> source,
+            int pageIndex,
+            int pageSize,
+            string sortColumn = null,
+            string sortOrder = null,
+            string filterColumn = null,
+            string filterQuery = null)
         {
+            if (!string.IsNullOrEmpty(filterColumn) && !string.IsNullOrEmpty(filterQuery) && IsValidProperty(filterColumn))
+            {
+                source = source.Where(
+                    string.Format("{0}.Contains(@0)",
+                    filterColumn),
+                    filterQuery);
+            }
+
             var count = await source.CountAsync();
 
             if (!string.IsNullOrEmpty(sortColumn) && IsValidProperty(sortColumn))
@@ -52,7 +86,16 @@ namespace WorldCities.Data
                 .Take(pageSize);
 
             var data = await source.ToListAsync();
-            return new ApiResult<T>(data, count, pageIndex, pageSize, sortColumn, sortOrder);
+
+            return new ApiResult<T>(
+                data,
+                count,
+                pageIndex,
+                pageSize,
+                sortColumn,
+                sortOrder,
+                filterColumn,
+                filterQuery);
         }
 
         /// <summary>
@@ -102,6 +145,15 @@ namespace WorldCities.Data
         /// Sorting Order ("ASC", "DESC" or null if none set)
         /// </summary>
         public string SortOrder { get; set; }
+        /// <summary>
+        /// Filter Column name (or null if none set)
+        /// </summary>
+        public string FilterColumn { get; set; }
+        /// <summary>
+        /// Filter Query string
+        /// (to be used within the given FilterColumn)
+        /// </summary>
+        public string FilterQuery { get; set; }
 
         public bool HasPreviousPage
         {
