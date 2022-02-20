75abafce6872702a22af4486ff539ddfb3bbfab1
Vladimir Ivanov <vgivanov@gmail.com>
2022-02-20 21:20:10 +0200

    SendGrid Integration Fix Email

--- WorldCities/WorldCities/Program.cs
+++ WorldCities/WorldCities/Program.cs
@@ -65,9 +65,9 @@ namespace WorldCities
         {
             var apiKey = Environment.GetEnvironmentVariable("WorldCities");
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("test@example.com", "Example User");
+            var from = new EmailAddress("sfi@abv.bg", "Example User");
             var subject = "Sending with SendGrid is Fun";
-            var to = new EmailAddress("vladosfi@gmail.com", "Example User");
+            var to = new EmailAddress("sfi@abv.bg", "Example User");
             var plainTextContent = "and easy to do anywhere, even with C#";
             var htmlContent = "<strong>and easy to do anywhere, even with C#</strong>";
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
