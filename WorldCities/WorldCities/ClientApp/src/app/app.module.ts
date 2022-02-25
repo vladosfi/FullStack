a821a551ae67b3ce3a2b61b0eac14ce669019b69
vladosfi <vladosfi@gmail.com>
2022-02-25 17:30:21 +0200

    PWA prepair

--- WorldCities/WorldCities/ClientApp/src/app/app.module.ts
+++ WorldCities/WorldCities/ClientApp/src/app/app.module.ts
@@ -17,7 +17,8 @@ import { CountryEditComponent } from './countries/country-edit.component';
 
 import { ApiAuthorizationModule } from 'src/api-authorization/api-authorization.module';
 import { AuthorizeInterceptor } from 'src/api-authorization/authorize.interceptor';
-
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -38,6 +39,16 @@ import { AuthorizeInterceptor } from 'src/api-authorization/authorize.intercepto
     BrowserAnimationsModule,
     AngularMaterialModule,
     ReactiveFormsModule,
+    ServiceWorkerModule.register(
+      'ngsw-worker.js', {
+      enabled: environment.production
+    }),
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [
     {
